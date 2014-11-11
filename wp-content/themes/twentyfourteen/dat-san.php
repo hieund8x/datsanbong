324329e7cd0a38f0e7e2c6646f863962e29f4922
HIEUND <hieund@smartosc.com>
2014-11-11 15:32:03 +0700

    update

--- wp-content/themes/twentyfourteen/dat-san.php
+++ wp-content/themes/twentyfourteen/dat-san.php
@@ -50,31 +50,31 @@
                 <h1 class="">Mời Bạn Chọn Sân:</h1>
                 <div class="list">
                     <a class="btn-box-san" href="javascript:void(0)" value="1" loaisan="Sân cỏ" onclick="chonSan($(this))">
-                        <img style="width: 100%; height: 130px" src="<?php echo get_template_directory_uri() . '/modulesanbong/css/images/san-bong.jpg' ?>" alt="" />
-                        <span class="san-title">Sân 1</span>
+                        <img style="width: 100%; height: 170px" src="<?php echo get_template_directory_uri() . '/modulesanbong/css/images/san-bong.jpg' ?>" alt="" />
+                        <span class="san-title">Sân 1 - Sân cỏ</span>
                     </a>
                     <a class="btn-box-san" href="javascript:void(0)" value="2" loaisan="Sân đất" onclick="chonSan($(this))">
-                        <img style="width: 100%; height: 130px" src="<?php echo get_template_directory_uri() . '/modulesanbong/css/images/san-bong.jpg' ?>" alt="" />
-                        <span class="san-title">Sân 2</span>
+                        <img style="width: 100%; height: 170px" src="<?php echo get_template_directory_uri() . '/modulesanbong/css/images/san-bong.jpg' ?>" alt="" />
+                        <span class="san-title">Sân 2 - Sân đất</span>
                     </a>
                     <a class="btn-box-san" href="javascript:void(0)" value="3" loaisan="Sân đất" onclick="chonSan($(this))">
-                        <img style="width: 100%; height: 130px" src="<?php echo get_template_directory_uri() . '/modulesanbong/css/images/san-bong.jpg' ?>" alt="" />
-                        <span class="san-title">Sân 3</span>
+                        <img style="width: 100%; height: 170px" src="<?php echo get_template_directory_uri() . '/modulesanbong/css/images/san-bong.jpg' ?>" alt="" />
+                        <span class="san-title">Sân 3 - Sân đất</span>
                     </a>
                     <a class="btn-box-san" href="javascript:void(0)" value="4" loaisan="Sân đất" onclick="chonSan($(this))">
-                        <img style="width: 100%; height: 130px" src="<?php echo get_template_directory_uri() . '/modulesanbong/css/images/san-bong.jpg' ?>" alt="" />
-                        <span class="san-title">Sân 4</span>
+                        <img style="width: 100%; height: 170px" src="<?php echo get_template_directory_uri() . '/modulesanbong/css/images/san-bong.jpg' ?>" alt="" />
+                        <span class="san-title">Sân 4 - Sân đất</span>
                     </a>
                 </div>
                 <div class="clear"></div>
-                <div class="show-selected">
+                <div class="show-selected" style="display: none;">
                     <span>Sân đã chọn: </span><span id="show-san">Chưa chọn</span>
                 </div>
                 <input type="hidden" id="so-san" name="so_san" value="0" messerr="Vui lòng chọn Sân để tiếp tục." />
             </div>
-            <div class="action">
-                <button class="btn next-step" type="button" onclick="nextStep($('#so-san'),$('#step-stadium'),$('#step-date'))">Tiếp Tục</button>
-            </div>
+<!--            <div class="action">-->
+<!--                <button class="btn next-step" type="button" onclick="nextStep($('#so-san'),$('#step-stadium'),$('#step-date'))">Tiếp Tục</button>-->
+<!--            </div>-->
             <div class="clear"></div>
         </div>
         <div id="step-date" style="display: none;">
@@ -94,15 +94,27 @@
             <div class="clear"></div>
         </div>
         <div id="step-time" style="display: none;">
+            <h1 class="">Mời Bạn Chọn Khung Giờ:</h1>
             <div class="step-content">
-                <a class="btn-box" href="javascript:void(0)" value="1" loaisan="Sân cỏ" onclick="chonGio($(this))">12h30 - 14h00</a>
-                <a class="btn-box" href="javascript:void(0)" value="1" loaisan="Sân cỏ" onclick="chonGio($(this))">14h00 - 15h30</a>
+                <a class="btn-box" href="javascript:void(0)" value="1" onclick="chonGio($(this))">12h30 - 14h00</a>
+                <a class="btn-box" href="javascript:void(0)" value="2"  onclick="chonGio($(this))">14h00 - 15h30</a>
+                <a class="btn-box" href="javascript:void(0)" value="3"  onclick="chonGio($(this))">15h30 - 17h00</a>
+                <a class="btn-box" href="javascript:void(0)" value="4"  onclick="chonGio($(this))">17h00 - 18h30</a>
+                <a class="btn-box" href="javascript:void(0)" value="5"  onclick="chonGio($(this))">18h30 - 20h00</a>
+                <a class="btn-box" href="javascript:void(0)" value="6"  onclick="chonGio($(this))">20h00 - 21h30</a>
+                <a class="btn-box" href="javascript:void(0)" value="7"  onclick="chonGio($(this))">21h30 - 23h00</a>
             </div>
+            <input type="hidden" id="gio" name="gio" value="" messerr="Vui lòng chọn Khung giờ để tiếp tục" />
             <div class="action">
                 <button class="btn prev-step" type="button" onclick="backStep($('#step-date'),$('#step-time'))">Quay lại</button>
-                <button class="btn next-step" type="button" onclick="nextStep($('#ngay-thang'),$('#step-date'),$('#step-time'))">Tiếp Tục</button>
             </div>
             <div class="clear"></div>
+        </div>
+        <div id="step-info" style="display: none;">
+            <h1>Vui điền thông tin liên hệ của bạn:</h1>
+            <div class="action">
+                <button class="btn prev-step" type="button" onclick="backStep($('#step-time'),$('#step-info'))">Quay lại</button>
+            </div>
         </div>
     </form>
 </div>
@@ -125,10 +137,15 @@
         function chonSan(el){
             $('#so-san').val(el.attr('value'));
             $('#show-san').html('Sân '+el.attr('value')+' - Loại sân: '+el.attr('loaisan'));
+            nextStep($('#so-san'),$('#step-stadium'),$('#step-date'));
         }
         function chonNgay(value,day){
             $('#ngay-thang').val(value);
             $('#show-date').html(day +' - '+value);
+        }
+        function chonGio(el){
+            $('#gio').val(el.attr('value'));
+            nextStep($('#gio'),$('#step-time'),$('#step-info'));
         }
         function nextStep(elVal,elHide,elShow){
             if(elVal.val()=='' || elVal.val()==0){
